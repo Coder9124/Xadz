6ea4b3db5b447ef77c07eea28fb3c6d8b6b59834
_09ms_ <134145400+Coder9124@users.noreply.github.com>
2023-12-10 21:34:59 +0530

    Update Profile.tsx

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -63,9 +63,10 @@ const Profile = () => {
             </div>
 
             <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
+              
               <StatBlock value={currentUser.posts.length} label="Posts" />
-              <StatBlock value={20} label="Followers" />
-              <StatBlock value={20} label="Following" />
+              {/* <StatBlock value={20} label="Followers" />
+              <StatBlock value={20} label="Following" /> */}
             </div>
 
             <p className="small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm">
@@ -93,7 +94,7 @@ const Profile = () => {
             </div>
             <div className={`${user.id === id && "hidden"}`}>
               <Button type="button" className="shad-button_primary px-8">
-                Follow
+                View
               </Button>
             </div>
           </div>
