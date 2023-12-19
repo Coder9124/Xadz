21bb9497280200df6fc4dd0fc851cbcabd640cec
_09ms_ <134145400+Coder9124@users.noreply.github.com>
2023-12-19 21:43:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
-import { CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
+import { CreatePost, EditPost, explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
@@ -21,7 +21,7 @@ const App = () => {
             {/* private routes */}
             <Route element={<RootLayout />}>
                 <Route index element={<Home />} />
-                <Route path='/explore' element={<Explore />} />
+                <Route path='/explore' element={<explore />} />
                 <Route path='/saved' element={<Saved />} />
                 <Route path='/create-post' element={<CreatePost />} />
                 <Route path='/update-post/:id' element={<EditPost />} />
